112a74312ef68a4e4c4298d4242319155067b832
hsmtDP-T <ittuyo.hashi244@gmail.com>
2025-09-12 01:16:28 +0900

    動作確認済み　店アカウントのAPI、一般ユーザーのAPI見直し

--- back/src/store/storeController.ts
+++ back/src/store/storeController.ts
@@ -1,6 +1,79 @@
 import express, { Request, Response } from 'express';
 import * as storeService from './storeService';
+import { firestore } from 'firebase-admin';
 
 const router = express.Router();
+
+//店アカウント作成（DBのパスワードハッシュ化したいだけやからフロント叩かなくていいよ）
+router.post("/sgin", async (req: Request,res: Response) =>{
+    const {email, pass} = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: "emailを入力してください"});
+    };
+
+    if (!pass) {
+        return res.status(400).json({ message: "パスワードを入力してください"});
+    };
+
+    try {
+        await storeService.storeSginup(email,pass);
+        res.status(200).json({ message: "店アカウント作成成功",}); 
+    } catch (error) {
+        res.status(500).json({ message: "店アカウント作成失敗"},);
+        console.log(error)
+    }
+});
+
+//店アカウントログイン処理
+router.post("/", async (req: Request,res: Response) =>{
+    const {email, pass} = req.body;
+
+    if (!email) {
+        res.status(400).json({ message: "emailを入力してください"});
+    };
+
+    if (!pass) {
+        res.status(400).json({ message: "パスワードを入力してください"});
+    };
+
+    try {
+        const result = await storeService.storeLogin(email,pass);
+        res.status(200).json({ message: "店アカウントログイン成功", token:result.token}); 
+    } catch (error) {
+        res.status(500).json({ message: "店アカウントログイン失敗"});
+    }
+});
+//店アカウントの情報変更
+router.patch("/", async (req: Request, res: Response) => {
+    const authHeader = req.headers.authorization;
+    const {name,percent,tag,position} = req.body;
+    const data: any = {}
+    if (name !== undefined) data.name = name;
+    if (percent !== undefined) data.percent = percent;
+    if (tag !== undefined) data.tag = tag;
+    if (position !== undefined) {
+        data.position = new firestore.GeoPoint(
+            position.latitude,
+            position.longitude
+        );
+    }
+
+    if (!authHeader) {
+        return res.status(401).json({ message: "Authorization ヘッダーがありません" });
+    };
+
+    const token = authHeader.split(" ")[0];
+    if (!token) {
+        return res.status(401).json({ message: "トークンが見つかりません" });
+    };
+    
+    try {
+        await storeService.storeUpdate(token,data);
+        res.status(200).json({ message: "store情報更新完了"});
+    } catch (error) {
+        res.status(500).json({ message: "store情報更新失敗"});
+    }
+});
 
 export default router;
--- back/src/store/storeService.ts
+++ back/src/store/storeService.ts
@@ -1,1 +1,57 @@
-import { db, auth } from "../firebase/firebase";
+import { db } from "../firebase/firebase";
+import bcrypt from "bcryptjs";
+import { firestore } from "firebase-admin";
+import jwt from "jsonwebtoken";
+
+export const storeSginup = async (email: string, pass: string) => {
+    const randomNum = 10;
+    const hashPass = await bcrypt.hash(pass, randomNum)
+    await db.collection('stores').add({
+        email,
+        pass: hashPass,
+    });
+};
+
+export const storeLogin = async (email: string, pass: string) => {
+    
+    const checkEmail = await db.collection('stores').where("email","==",email).get();
+
+    if (checkEmail.empty) {
+        throw new Error("Emailが違います");
+    };
+
+    const doc = checkEmail.docs[0];
+    const data = doc.data();
+    
+    const checkPass = await bcrypt.compare(pass, data.pass);
+    if (!checkPass) {
+        throw new Error("パスワードが違います");
+    }
+
+    const token = jwt.sign(
+        { id: doc.id, email: data.email },
+        process.env.JWT_SECRET!,
+        {expiresIn: "1h"}           
+    );
+
+    return { token };
+};
+
+export interface storeUpdateData {
+    name?: string;
+    percent?: number;
+    tag?: string[];
+    position?: { latitude: number; longitude: number };
+}
+
+export const storeUpdate = async (token:string, data: storeUpdateData) => {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string; email: string };
+    const updateData: any = { ...data };
+    if (data.position) {
+        updateData.position = new firestore.GeoPoint(
+            data.position.latitude,
+            data.position.longitude
+        );
+    }
+    return await db.collection('stores').doc(decoded.id).update(updateData);
+}
