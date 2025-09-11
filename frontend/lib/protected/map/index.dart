f7ff03819a4198e0fb076158b3872a8571e05dd5
矢野温人 <hal@yanoatsushininnoMacBook-Air.local>
2025-09-12 02:35:24 +0900

    add: navbar作成

---
+++ frontend/lib/protected/map/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'layout.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: MapLayout.bgColor,
+
+    );
+  }
+}
+
