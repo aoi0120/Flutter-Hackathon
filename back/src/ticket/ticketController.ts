a5f95ef860cdca813d211600f634568efa9a4e8f
hsmtDP-T <ittuyo.hashi244@gmail.com>
2025-09-12 02:58:28 +0900

    動作確認済み　店アカウントのJWTがある場合のチケット新規登録API

--- back/src/ticket/ticketController.ts
+++ back/src/ticket/ticketController.ts
@@ -3,5 +3,32 @@ import * as ticketService from './ticketService';
 
 const router = express.Router();
 
+//チケットの新規追加
+router.post("/", async ( req: Request, res: Response ) => {
+    const authHeader = req.headers.authorization;
+    const { rank_id, prize, expiration_at }  = req.body;
+
+    if ( !rank_id || !prize || !expiration_at ) {
+        return res.status(400).json({ message: "必要な情報が抜けてます"});
+    }
+    const data = { rank_id, prize, expiration_at: new Date(expiration_at) };
+
+    if (!authHeader) {
+        return res.status(401).json({ message: "Authorization ヘッダーがありません" });
+    };
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "トークンが見つかりません" });
+    };
+
+    try {
+        await ticketService.createTicket( token, data );
+        res.status(200).json({ message: "チケットの新規追加成功"});
+    } catch (error) {
+        res.status(500).json({ message: "チケットの新規追加失敗"});
+        console.log(error);
+    }
+});
 
 export default router;
--- back/src/ticket/ticketService.ts
+++ back/src/ticket/ticketService.ts
@@ -1,1 +1,22 @@
 import { db, auth } from "../firebase/firebase";
+import jwt from "jsonwebtoken";
+
+export interface ticketData  {
+    rank_id: number,
+    prize: string,
+    expiration_at: Date,
+}
+
+export const createTicket = async ( token:string, data: ticketData ) => {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string; email: string };
+    const ticket = await db.collection('tickets').add({
+        store_id: decoded.id,
+        rank_id: data.rank_id,
+        prize: data.prize,
+        expiration_at: new Date(data.expiration_at),
+        effective: true
+    });
+    return {
+        ticket_id: ticket.id,
+    };
+}
