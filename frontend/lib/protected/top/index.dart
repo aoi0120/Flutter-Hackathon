cb7553e5bafbe3a55e951bfd1fefbf5e1729d253
hal <halt0413@gmail.com>
2025-09-12 03:27:23 +0900

    refactor: 不要な状態管理とメソッドを削除

--- frontend/lib/protected/top/index.dart
+++ frontend/lib/protected/top/index.dart
@@ -7,33 +7,6 @@ import '../../component/ui/ellipses/ellipses.dart';
 class TopPage extends StatefulWidget {
   const TopPage({super.key});
   @override
-  State<TopPage> createState() => _TopPageState();
-}
-
-class _TopPageState extends State<TopPage> {
-  int _indexFromLocation(BuildContext context) {
-    final loc = GoRouterState.of(context).uri.toString();
-    if (loc.startsWith('/')) return 1;
-    if (loc.startsWith('/')) return 2;
-    return 0;
-  }
-
-  void _onBottomTap(int i) {
-    switch (i) {
-      case 0:
-        context.go('/');
-        break;
-      //TODO: ルーティングの設定
-      case 1:
-        // context.go('/');
-        break;
-      case 2:
-        // context.go('/');
-        break;
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     final currentIndex = _indexFromLocation(context);
 
