58f66749fcb2f2e68c008dad5ba855e00a8568c5
shiomoto-kei <sk1918ds@gmail.com>
2025-09-12 00:44:32 +0900

    動作確認済み　タグ追加

--- back/src/tag/tagService.ts
+++ back/src/tag/tagService.ts
@@ -1,1 +1,9 @@
 import { db, auth } from "../firebase/firebase";
+
+export const createTag = async (tagName:string,tagId:number) => {
+    //firebaseのtagsにタグを追加
+    await db.collection('tags').add({
+        name: tagName,
+        tag_id: tagId,
+    });    
+} 
