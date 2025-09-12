ceaf79104bced5323c39af56ecbdb46a5b2a2e99
Jin-Nakamyouzu <3n3.nazu3.Naka@gmail.com>
2025-09-13 01:23:43 +0900

    fix: ボタンカラーをグレーからホワイトに変更

--- frontend/lib/protected/top/_component/itemButton/item_button_style.dart
+++ frontend/lib/protected/top/_component/itemButton/item_button_style.dart
@@ -12,7 +12,7 @@ class ItemButtonStyle {
   static const double buttonSize = 60.0; // 直径
   static const double iconSize = 30.0; // アイコンのサイズ
   static const double spacing = 8.0; // ボタンとテキストの間隔
-  static const Color buttonColor = Colors.grey; // ボタンカラー
+  static const Color buttonColor = Colors.white; // ボタンカラー
 
   // アイコン画像(３点リーダー)
   static const String ellipsesIcon = 'assets/images/ellipses.png';
