2572f9b479c53ced7e0e015b9723c190a50b9714
hal <halt0413@gmail.com>
2025-09-12 17:59:19 +0900

    add: gachaboxの追加

--- frontend/lib/protected/top/_component/gacha/gacha.dart
+++ frontend/lib/protected/top/_component/gacha/gacha.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class Gacha extends StatelessWidget {
-  const Gacha({super.key});
+class Gacha extends StatefulWidget {
+  const Gacha({
+    super.key,
+    this.onCompleted,
+  });
+
+  final VoidCallback? onCompleted;
+
+  @override
+  State<Gacha> createState() => _GachaState();
+}
+
+class _GachaState extends State<Gacha> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        widget.onCompleted?.call();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Lottie.asset(
-        'assets/data/data.json',
-        repeat: false,
-        animate: true,
-        reverse: false,           
-        fit: BoxFit.contain,
-      ),
+    return Lottie.asset(
+      'assets/data/data.json',
+      controller: _controller,
+      onLoaded: (comp) {
+        _controller
+          ..duration = comp.duration
+          ..forward();
+      },
+      repeat: false,
+      fit: BoxFit.contain,
     );
   }
 }
--- frontend/lib/protected/top/index.dart
+++ frontend/lib/protected/top/index.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import './_component/gacha/gacha.dart';
+import './_component/gachabox/gachabox.dart';
 import './_component/gachabar/gachabar.dart';
 import './_component/gachabar/gachabar_styles.dart';
 import 'layout.dart';
 
-class TopPage extends StatelessWidget {
+class TopPage extends StatefulWidget {
   const TopPage({super.key});
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  bool _animationDone = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +22,15 @@ class TopPage extends StatelessWidget {
       child: Stack(
         alignment: Alignment.center,
         children: [
-          const Center(child: Gacha()),
+          const Center(child: GachaBox()),
+          if (!_animationDone)
+            Center(
+              child: Gacha(
+                onCompleted: () {
+                  setState(() => _animationDone = true);
+                },
+              ),
+            ),
           Positioned(
             bottom: BarStyles.bottomOffset,
             left: 0,
