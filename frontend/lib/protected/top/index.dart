c0126f30ca49d75222ca98a93c9c5e6baa9946ed
Jin-Nakamyouzu <3n3.nazu3.Naka@gmail.com>
2025-09-12 17:06:20 +0900

    "お知らせ"と"所持チケ"のテキストの表示

--- frontend/lib/protected/top/index.dart
+++ frontend/lib/protected/top/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './_component/gacha/gacha.dart';
+import './_component/gacha/item_button.dart';
 import 'layout.dart';
 
 class TopPage extends StatelessWidget {
@@ -7,10 +8,16 @@ class TopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: TopLayout.bgColor,
-      child: const Gacha(),
+    return Scaffold(
+      backgroundColor: TopLayout.bgColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const ItemButton(),
+            Expanded(child: const Gacha()),
+          ],
+        ),
+      ),
     );
   }
 }
-
