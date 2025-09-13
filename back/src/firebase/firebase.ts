0b8487cba792eaa91adc5a2bf992ea723ebdf541
AoiXD <oosabaaoi@gmail.com>
2025-09-14 06:19:28 +0900

    fix: Firebaseの環境変数名を小文字に変更

--- back/src/firebase/firebase.ts
+++ back/src/firebase/firebase.ts
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const requiredEnvKeys = [
-    "FIREBASE_TYPE",
-    "FIREBASE_PROJECT_ID",
-    "FIREBASE_PRIVATE_KEY_ID",
-    "FIREBASE_PRIVATE_KEY",
-    "FIREBASE_CLIENT_EMAIL",
-    "FIREBASE_CLIENT_ID",
-    "FIREBASE_AUTH_URI",
-    "FIREBASE_TOKEN_URI",
-    "FIREBASE_AUTH_PROVIDER_X509_CERT_URL",
-    "FIREBASE_CLIENT_X509_CERT_URL",
-    "FIREBASE_UNIVERSE_DOMAIN",
+    "type",
+    "project_id",
+    "private_key_id",
+    "private_key",
+    "client_email",
+    "client_id",
+    "auth_uri",
+    "token_uri",
+    "auth_provider_x509_cert_url",
+    "client_x509_cert_url",
+    "universe_domain",
 ];
 
 const missing = requiredEnvKeys.filter((k) => !process.env[k]);
@@ -22,23 +22,23 @@ if (missing.length > 0) {
     throw new Error(`環境変数が不足しています: ${missing.join(", ")}`);
 }
 
-const privateKeyRaw = process.env.FIREBASE_PRIVATE_KEY as string;
+const privateKeyRaw = process.env.private_key as string;
 const privateKey = privateKeyRaw
     .replace(/\\n/g, "\n")
     .replace(/\r\n/g, "\n");
 
 const serviceAccount = {
-    type: process.env.FIREBASE_TYPE as string,
-    project_id: process.env.FIREBASE_PROJECT_ID as string,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID as string,
+    type: process.env.type as string,
+    project_id: process.env.project_id as string,
+    private_key_id: process.env.private_key_id as string,
     private_key: privateKey,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL as string,
-    client_id: process.env.FIREBASE_CLIENT_ID as string,
-    auth_uri: process.env.FIREBASE_AUTH_URI as string,
-    token_uri: process.env.FIREBASE_TOKEN_URI as string,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL as string,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL as string,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN as string,
+    client_email: process.env.client_email as string,
+    client_id: process.env.client_id as string,
+    auth_uri: process.env.auth_uri as string,
+    token_uri: process.env.token_uri as string,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url as string,
+    client_x509_cert_url: process.env.client_x509_cert_url as string,
+    universe_domain: process.env.universe_domain as string,
 };
 
 admin.initializeApp({
