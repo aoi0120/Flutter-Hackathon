094e78c4f32184513eb9ae3b5a8d050ffa83f0ce
hal <halt0413@gmail.com>
2025-09-14 05:55:49 +0900

    chore: ignore env.dart (untrack local-only env)

--- frontend/lib/config/env.dart
+++
@@ -1,7 +0,0 @@
-class Env {
-  static const apiBaseUrl = 'http://127.0.0.1:8080';
-  static const ticketsBase = '';   
-  static String _withSlash(String s) => s.endsWith('/') ? s : '$s/';
-  static String get tickets => _withSlash(ticketsBase.isNotEmpty ? ticketsBase : '$apiBaseUrl/api/tickets');
-}
-
