afea6ff1fa4a97a0c438c1c06aa4923ed98d1ce4
AoiXD <oosabaaoi@gmail.com>
2025-09-14 06:10:49 +0900

    fix: Firebaseのサービスアカウント変数名を修正

--- back/src/firebase/firebase.ts
+++ back/src/firebase/firebase.ts
@@ -1,10 +1,9 @@
 import * as admin from "firebase-admin";
 import dotenv from "dotenv";
-import fs from "fs";
 
 dotenv.config();
 
-const serviceAccountPath = {
+const serviceAccount = {
     type: process.env.FIREBASE_TYPE!,
     project_id: process.env.FIREBASE_PROJECT_ID!,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID!,
@@ -18,13 +17,9 @@ const serviceAccountPath = {
     universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN!,
 }
 
-if (!serviceAccountPath) {
+if (!serviceAccount) {
     throw new Error(".envにキーがない");
 }
-
-const serviceAccount = JSON.parse(
-    fs.readFileSync(JSON.stringify(serviceAccountPath), "utf-8")
-);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
