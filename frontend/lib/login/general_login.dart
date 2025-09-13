bdb3b8d4846bc6ab93b8d0fdb75c691fc7656e1c
hal <halt0413@gmail.com>
2025-09-13 14:33:38 +0900

    ログイン画面整える

--- frontend/lib/login/general_login.dart
+++ frontend/lib/login/general_login.dart
@@ -3,58 +3,79 @@ import 'general_login_style.dart';
 import '../auth.dart';
 
 class GeneralLogin extends StatelessWidget {
-  const GeneralLogin({
-    super.key,
-    // required this.text,
-    // this.onPressed,
-  });
-
-  // final String text;
+  const GeneralLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // 上部コンテナー
-          Expanded(
-            flex: 1,
-            child: Container(
-              child: const Center(child: GeneralScreenPicture()),
-            ),
-          ),
-          // 下部コンテナー
-          Expanded(
-            flex: 1,
-            child: Container(
-              child: Column(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 上部コンテナー
+            Expanded(
+              flex: 1,
+              child: Stack(
                 children: [
-                  // Google認証ボタン
-                  InkWell(
-                    onTap: () => {print('Google認証ボタンを押したよ'), auth.value = true},
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: Image.asset('assets/images/glogin.png'),
+                  Center(
+                    child: Image.asset(
+                      'assets/images/ishibashi.png',
+                      width: 300,
+                      fit: BoxFit.contain,
                     ),
                   ),
-                  SizedBox(height: 20.0),
-                  // 商店街の方専用ログイン画面
+                  Positioned(
+                    top: -10,
+                    right: -5,
+                    child: Image.asset(
+                      'assets/images/gatya.png',
+                      width: 200,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // 下部コンテナー（
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                 InkWell(
+                      onTap: () {
+                        print('Google認証ボタンを押したよ');
+                        auth.value = true;
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: Image(
+                          image: AssetImage('assets/images/glogin.png'),
+                          width: 300,
+                          // height: 60,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  const SizedBox(height: 20.0),
                   InkWell(
                     onTap: () => print('商店街の方専用ログイン画面へ'),
-                    child: Padding(
+                    child: const Padding(
                       padding: EdgeInsets.all(8.0),
                       child: Text(
                         '商店街の方はこちら',
-                        style: GeneralScreenPicture.textStyle,
+                        style: GeneralLoginStyle.linkText,
                       ),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
+
--- frontend/lib/login/general_login_style.dart
+++ frontend/lib/login/general_login_style.dart
@@ -1,27 +1,10 @@
 import 'package:flutter/material.dart';
 
-// ishibashi.pngとgatya.pngを組み合わせたもの
-class GeneralScreenPicture extends StatelessWidget {
-  const GeneralScreenPicture({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Image.asset('assets/images/ishibashi.png'),
-        Positioned(
-          top: 0,
-          right: 0,
-          child: Image.asset('assets/images/gatya.png'),
-        ),
-      ],
-    );
-  }
-
-  static const TextStyle textStyle = TextStyle(
+class GeneralLoginStyle {
+  static const TextStyle linkText = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.bold,
     color: Colors.grey,
   );
 }
+
